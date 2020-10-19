219073cd592ec78efabb148816a59c9ffe86d279
Jeremy Futral <jfutral@fanatics.com>
2020-10-19 14:15:51 -0400

    Script cleanup

--- sync_settings.sh
+++ sync_settings.sh
@@ -14,9 +14,19 @@ username=$(whoami)
 mkdir -p ~/${VIM_DIR}/after
 mkdir -p ~/${VIM_DIR}/autoload
 
+# create helper folders
+mkdir -p ~/.local/share/nvim/
+mkdir -p ~/.local/vim_scripts/
+
 # deploy addons
 cp -fr after/* ~/${VIM_DIR}/after/
 cp -fr autoload/* ~/${VIM_DIR}/autoload/
+
+# deploy helper scripts
+cp -fr scripts/* ~/.local/vim_scripts/
+
+# make scripts executable
+chmod +x ~/.local/vim_scripts/*
 
 # deploy the new vimrc file
 cp ~/.vimrc ~/.vimrc.bak
