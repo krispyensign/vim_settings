bbd2a001762e3687d7aa160528d5bc22647d7694
Jeremy Futral <jfutral@fanatics.com>
2023-02-20 15:33:05 -0500

    formatting

--- build_lsp.sh
+++ build_lsp.sh
@@ -1,16 +1,16 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 # select vim dir based on OS
 VIM_DIR=".vim"
 if [[ "msys" == $OSTYPE ]]; then
   VIM_DIR="vimfiles"
-fi  
-
-# Get the username and plugins
+fi
 username=$(whoami)
 
+echo "building..."
 cd ./plugins/YouCompleteMe
 git submodule update --init --recursive
-./install.py --all
+./install.py --all --verbose
 
+echo "deploying..."
 rm -fr ~/${VIM_DIR}/pack/${username}/start/YouCompleteMe
 cp -fr ../YouCompleteMe/ ~/${VIM_DIR}/pack/${username}/start/YouCompleteMe
--- sync.sh
+++ sync.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-
 function printh() {
 	printf "${GREEN}= $1 =${NC}\n"
 } 
@@ -20,8 +19,8 @@ function bluey() {
 # setup variables
 VIM_DIR=".vim"
 if [[ "msys" == $OSTYPE ]]; then
-  VIM_DIR="vimfiles"
-fi  
+	VIM_DIR="vimfiles"
+fi
 username=$(whoami)
 plugins=$(grep '^http.*$' plugins_list.txt)
 plugin_names=$(echo "${plugins}" | cut -d'/' -f5)
@@ -31,9 +30,9 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 printh "syncing the following plugins"
-printf "${YELLOW}${plugin_names}"
+printf "${YELLOW}${plugin_names}\n"
 
-printh "creating directory structure"
+printh "removing and creating new directory structure"
 mkdir -p plugins ~/${VIM_DIR} 
 pushd $(pwd) > /dev/null
 	cd ~/${VIM_DIR}/
@@ -69,17 +68,17 @@ pushd $(pwd) > /dev/null
 		prints "processing ${plugin_name}"
 		if [[ -d "${plugin_name}" ]]; then
 			pushd $(pwd) > /dev/null
-			prints "pulling latest ${plugin_name}"
-			cd ${plugin_name}
-			bluey
-			git pull || printe "something was not happy pulling"
-			popd > /dev/null
+				prints "pulling latest ${plugin_name}"
+				cd ${plugin_name}
+				bluey
+				git pull || printe "failed pulling ${plugin_name}"
+				popd > /dev/null
 			continue
 		fi
 
 		prints "cloning ${plugin_name}"
 		bluey
-		git clone ${plugin} || printe "something was not happy cloning"
+		git clone ${plugin} || printe "failed cloning ${plugin_name}"
 	done 
 
 	prints "deploying plugins directory"
