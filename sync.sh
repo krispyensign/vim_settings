1d6a3839393af40be919199c8525c21649e48731
krispyensign <futralj@gmail.com>
2023-02-20 19:04:52 +0000

    formatting

--- sync.sh
+++ sync.sh
@@ -11,52 +11,47 @@ username=$(whoami)
 plugins=$(cat plugins_list.txt)
 
 echo "=creating directory structure="
-chmod -R 777 ~/${VIM_DIR}/pack/*
-rm -fr								\
-	~/${VIM_DIR}/after 					\
-	~/${VIM_DIR}/plugin 					\
-	~/${VIM_DIR}/autoload 					\
-	~/${VIM_DIR}/pack/${username}/start/
-mkdir -p 							\
-	plugins/ 						\
-	~/${VIM_DIR}/after 					\
-	~/${VIM_DIR}/plugin 					\
-	~/${VIM_DIR}/autoload 					\
-	~/${VIM_DIR}/pack/${username}/start/
+mkdir -p plugins
+pushd $(pwd)
+	cd ~/${VIM_DIR}/
+	chmod -R 777 pack/
+	rm -fr after plugin autoload pack/${username}/start/
+	mkdir -p after plugin autoload pack/${username}/start/
+popd
 
 pushd $(pwd)
-echo "=processing plugins="
-cd plugins/
-while IFS= read -r plugin; do
-	if [[ ${plugin} =~ ^#.*$|^[[:blank:]]*$ ]]; then
-		continue
-	fi
+	echo "=processing plugins="
+	cd plugins/
+	while IFS= read -r plugin; do
+		if [[ ${plugin} =~ ^#.*$|^[[:blank:]]*$ ]]; then
+			continue
+		fi
 
-	plugin_name=$(echo ${plugin} | cut -d'/' -f5)
-	echo "=processing ${plugin_name}="
-	if [[ -d "${plugin_name}" ]]; then
-		pushd $(pwd)
-		echo "=${plugin_name} already cloned. updating...="
-		cd ${plugin_name}
-		git pull || echo "something was not happy pulling"
-		popd
-		continue
-	fi
+		plugin_name=$(echo ${plugin} | cut -d'/' -f5)
+		echo "=processing ${plugin_name}="
+		if [[ -d "${plugin_name}" ]]; then
+			pushd $(pwd)
+			echo "=${plugin_name} already cloned. updating...="
+			cd ${plugin_name}
+			git pull || echo "something was not happy pulling"
+			popd
+			continue
+		fi
 
-	echo "=cloning ${plugin_name}="
-	git clone ${plugin} || echo "something was not happy cloning"
-done < ../plugins_list.txt
+		echo "=cloning ${plugin_name}="
+		git clone ${plugin} || echo "something was not happy cloning"
+	done < ../plugins_list.txt
 
-# sync plugins directory
-echo "=deploying plugins directory="
-cp -fr * ~/${VIM_DIR}/pack/${username}/start/
+	# sync plugins directory
+	echo "=deploying plugins directory="
+	cp -fr * ~/${VIM_DIR}/pack/${username}/start/
 popd
 
 # deploy rainbow
 pushd $(pwd)
-echo "=configuring rainbow="
-cd ~/${VIM_DIR}/pack/${username}/start/rainbow
-cp -fr plugin/ autoload/ ~/${VIM_DIR}/
+	echo "=configuring rainbow="
+	cd ~/${VIM_DIR}/pack/${username}/start/rainbow
+	cp -fr plugin/ autoload/ ~/${VIM_DIR}/
 popd
 
 # deploy other addons
