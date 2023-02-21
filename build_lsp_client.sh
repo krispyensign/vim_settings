3e7177704fb245647cd981d3565a66abc153150b
krispyensign <futralj@gmail.com>
2023-02-21 05:09:23 +0000

    fix dirs

--- build_lsp_client.sh
+++ build_lsp_client.sh
@@ -3,15 +3,16 @@
 source ./common.sh
 
 pushd $(pwd) > /dev/null
-cd ./plugins/YouCompleteMe
+cd ./${pack_folder}/YouCompleteMe
 printh "updating modules"
 git submodule update --init --recursive
 printh "building"
 ./install.py --all --verbose
 popd
 
-printh "deploying..."
+prints "deploying plugins directory"
 set -x
-chmod -R u+rw ${pack_folder}/YouCompleteMe
 rsync -pEr ./${pack_folder} ${user_pack_folder}
-ls -lah ${user_pack_folder}
+unset -x
+printh "deployed"
+ls ${user_pack_folder}
