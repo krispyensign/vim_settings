c4c2e479ec5573bc11e07139621df66893d8c197
krispyensign <futralj@gmail.com>
2023-02-21 04:47:25 +0000

    remove stuff

--- purge.sh
+++ purge.sh
@@ -6,13 +6,15 @@ VIM_DIR=".vim"
 if [[ "msys" == $OSTYPE ]]; then
   VIM_DIR="vimfiles"
 fi  
+username=$(whoami)
+pack_folder=pack/${username}/start/
+user_pack_folder=${vim_dir}${pack_folder}
 
 # Get the username and plugins
-username=$(whoami)
 echo "Resetting permissions..."
-chmod -R 777 ~/${VIM_DIR}/pack/*
+chmod -R 777 ${user_pack_folder}
 echo "Purging plugins in home..."
-rm -fr ~/${VIM_DIR}/pack/*
+rm -fr ${user_pack_folder}
 echo "Purging plugins in project..."
 rm -fr ./plugins
 echo "Done!"
