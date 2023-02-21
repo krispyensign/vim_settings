07da99541a49c0abfbe872732226afd000379417
krispyensign <futralj@gmail.com>
2023-02-21 05:55:39 +0000

    improve purge

--- purge.sh
+++ purge.sh
@@ -3,10 +3,10 @@
 source common.sh
 
 # Get the username and plugins
-printh "Resetting permissions..."
-chmod -R 777 ${user_pack_folder}
-printh "Purging plugins in home..."
-rm -fr ${user_pack_folder}
-printh "Purging plugins in project..."
-rm -fr ${pack_folder}
+printh "Resetting permissions"
+chmod -Rv 777 ${user_pack_folder}
+printh "Purging plugins in home"
+rm -frv ${user_pack_folder}
+printh "Purging plugins in project"
+rm -frv ${pack_folder}
 printh "Done!"
