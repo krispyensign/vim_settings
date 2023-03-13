ec9619471959829cb2560ed9b6b4d248f692db74
krispyensign <futralj@gmail.com>
2023-03-13 01:12:15 -0400

    removed plugins

--- get_plugins.sh
+++ get_plugins.sh
@@ -5,17 +5,17 @@ submodules=$(cat plugins_list.txt)
 plugin_paths=$(ls plugins/)
 for plugin in $plugin_paths; do
   git submodule deinit -f plugins/$plugin
-  git add --all
-  git rm -fr plugins/$plugin
-  rm -fr plugins/$plugin
-  rm -fr .git/modules/plugins/$plugin
+#  git add --all
+#  git rm -fr plugins/$plugin
+#  rm -fr plugins/$plugin
+#  rm -fr .git/modules/plugins/$plugin
 done
 
 cd plugins
 
-for submodule in $submodules; do
-  if [[ $submodule != \#* ]]; then
-    git submodule add -f $submodule
-  fi
-done
+# for submodule in $submodules; do
+#   if [[ $submodule != \#* ]]; then
+#     git submodule add -f $submodule
+#   fi
+# done
 
