d5c3dfe3260614a7f96ca1a07d6543b39944133b
krispyensign <futralj@gmail.com>
2023-03-13 01:12:18 -0400

    added cleanup of stale

--- sync.sh
+++ sync.sh
@@ -18,26 +18,44 @@ pushd $(pwd)
 	mkdir -p after plugin autoload pack/${username}/start/
 popd
 
+echo "=cleanup stale plugins="
+pushd $(pwd)
+	cd plugins/
+	for oldplugin in $(ls); do
+		found="0"
+		for truthplugin in $(echo "${plugin_names}"); do
+			if [[ oldplugin == truthplugin ]]; then
+				echo "*found ${oldplugin}"
+				found="1"
+			fi
+		done
+		if [[ ${found} == 0 ]]; then
+			echo "*purging ${oldplugin}="
+			rm -fr $oldplugin
+		fi
+	done
+popd
+
 echo "=processing plugins="
 pushd $(pwd)
 	cd plugins/
 	while IFS= read -r plugin; do
 		plugin_name=$(echo ${plugin} | cut -d'/' -f5)
-		echo "=processing ${plugin_name}="
+		echo "*processing ${plugin_name}"
 		if [[ -d "${plugin_name}" ]]; then
 			pushd $(pwd)
-			echo "=pulling latest ${plugin_name}="
+			echo "*pulling latest ${plugin_name}"
 			cd ${plugin_name}
 			git pull || echo "something was not happy pulling"
 			popd
 			continue
 		fi
 
-		echo "=cloning ${plugin_name}="
+		echo "*cloning ${plugin_name}"
 		git clone ${plugin} || echo "something was not happy cloning"
 	done < ../plugins_list.txt
 
-	echo "=deploying plugins directory="
+	echo "*deploying plugins directory"
 	cp -fr * ~/${VIM_DIR}/pack/${username}/start/
 popd
 
